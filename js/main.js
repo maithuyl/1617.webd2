c32de8a26f7b6d83bd77bf584232e4660b3a96b9
maithuyl <maithuyl@student.arteveldehs.be>
2017-05-26 17:24:45 +0200

    initial commit to the repo: README.md and .gitignore

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
-;(function() {
-  alert('Welcome to the GDM Web Template developed by drdynscript!');
-})();
+// slide out menu
+$(function(){
+
+    var menuState = false; 
+
+    var openMenu = function() {
+        $('.menu').animate({
+            left: "0"
+        }, 1000);
+    }
+
+    var closeMenu = function() {
+        $('.menu').animate({
+            left: "-18em"
+        }, 1000);
+    }
+
+    $(document).ready(function(){
+        $('.hamburgerContainer').on('click', function() {
+            if(!menuState) {
+                openMenu();
+                menuState = true;
+            } else {
+                closeMenu();
+                menuState = false;
+            }
+        });
+    }); 
+}); 
+
+
+//zoom function on projects
+
+
+$(document).ready(function(){
+    $('.imgZoom').hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
+  });
+
+
+						
